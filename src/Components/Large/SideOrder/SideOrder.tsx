7f96371aa2e25d4eeafd8d1a695472fbd4f6729a
doaa <73131139+doaa10@users.noreply.github.com>
2024-07-16 00:55:29 +0300

    sideContainer

--- src/Components/Large/SideOrder/SideOrder.tsx
+++ src/Components/Large/SideOrder/SideOrder.tsx
@@ -1,14 +1,52 @@
 import React from 'react'
 import styles from './SideOrder.module.css'
+import { RiVisaFill } from "react-icons/ri";
+import { IoCashOutline } from "react-icons/io5";
+import { BsQrCodeScan } from "react-icons/bs";
+import Button from '@/Components/Small/Button/Button';
+
 const SideOrder = () => {
   return (
     <div className={styles.sideOrder}>
         <div className={styles.sideOrder_container}>
-            <div className={styles.sideOrder_container_head}></div>
-            <div className={styles.sideOrder_container_orders}></div>
-            <div className={styles.sideOrder_container_payment}></div>
-            <div className={styles.sideOrder_container_method}></div>
-            <div className={styles.sideOrder_container_button}></div>
+            <div className={styles.sideOrder_container_head}>
+                <span>Order</span>
+                <span>#123</span>
+            </div>
+            <div className={styles.sideOrder_container_orders}>
+                
+            </div>
+            <div className={styles.sideOrder_container_payment}>
+                <span className={styles.sideOrder_container_payment_title}>Payment Summary</span>
+                <div>
+                    <span>sub-Total</span>
+                    <span>45$</span>
+                </div>
+
+                <div>
+                    <span>Tax</span>
+                    <span>0$</span>
+                </div>
+
+                <div>
+                    <span>Discount</span>
+                    <span>0$</span>
+                </div>
+
+                <div className={styles.total}>
+                    <span>Total</span>
+                    <span>45$</span>
+                </div>
+
+            </div>
+            <div className={styles.sideOrder_container_method}>
+                    <div><RiVisaFill size={26}/></div> 
+                    <div><IoCashOutline size={26}/></div>
+                    <div><BsQrCodeScan size={26}/></div>
+            </div>
+            <div className={styles.sideOrder_container_button}>
+                <Button type='popular' size='mediam' children='Proceed'/>
+            </div>
 
 
         </div>
--- src/app/(DataView)/Menu/page.tsx
+++ src/app/(DataView)/Menu/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './Page.module.css'
 import Categories from '@/Components/Small/Categories/Categories'
 import MenuGrid from '@/Components/Large/MenuGrid/MenuGrid'
+import SideOrder from '@/Components/Large/SideOrder/SideOrder'
 const Menu = () => {
   return (
     <div className={styles.menu}>
@@ -16,7 +17,7 @@ const Menu = () => {
 
             </div>
             <div className={styles.menu_container_rightOrder}>
-              
+                   <SideOrder/>
             </div>
       </div>
 
