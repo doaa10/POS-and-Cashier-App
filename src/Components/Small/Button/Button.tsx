923506180ab79626759ddfe0c5922682a175efc4
doaa <73131139+doaa10@users.noreply.github.com>
2024-07-16 23:53:35 +0300

    inventory header

--- src/Components/Small/Button/Button.tsx
+++ src/Components/Small/Button/Button.tsx
@@ -3,17 +3,19 @@ import styles from './Button.module.css';
 
 interface ButtonProps {
   size: string;
-  type: string; // [primary, success, danger]
+  type: string; 
   children: React.ReactNode;
   onClick?: () => void;
+  icon?: React.ReactNode;
+
 }
 
-const Button: React.FC<ButtonProps> = ({ size, type, children, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ size, type, children, onClick ,icon}) => {
   const typeClass = type === 'popular' ? styles.popular : styles[type];
 
   return (
     <button className={`${styles.button} ${typeClass} ${styles[size]}`} onClick={onClick}>
-      {children}
+      {icon}{children}
     </button>
   );
 }
--- src/app/(DataView)/Inventory/page.tsx
+++ src/app/(DataView)/Inventory/page.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import styles from './Page.module.css'
+import Button from '@/Components/Small/Button/Button'
+import { TiPlus } from "react-icons/ti";
+import { PiExportBold } from "react-icons/pi";
+
 const Inventory = () => {
   return (
-    <div>inventory</div>
+    <div className={styles.inventory}>
+        <div className={styles.inventory_container}>
+          <div className={styles.inventory_container_header}>
+              <div className={styles.inventory_container_header_title}>
+                <span>Inventory</span>
+              </div>
+              <div className={styles.inventory_container_header_actions}>
+              <Button type='secondary' size='meduim2' children='Export' icon={<PiExportBold/>} />
+
+                <Button type='popular' size='meduim' children='Add Product' icon={<TiPlus/>} />
+              </div>
+          </div>
+          <div className={styles.inventory_container_table}>
+
+          </div>
+        </div>
+
+    </div>
   )
 }
 
