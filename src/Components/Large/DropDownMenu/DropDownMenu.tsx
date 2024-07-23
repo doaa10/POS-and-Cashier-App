002ee31dcb69c9fe4f0c066638f9d5e4314169b5
doaa <73131139+doaa10@users.noreply.github.com>
2024-07-23 03:38:50 +0300

    dashboard dropdown menu

---
+++ src/Components/Large/DropDownMenu/DropDownMenu.tsx
@@ -0,0 +1,104 @@
+'use client'
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Menu, { MenuProps } from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import styles from './DropDownMenu.module.css';
+
+const StyledMenu = styled((props: MenuProps) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'right',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'right',
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  '& .MuiPaper-root': {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 120,
+    color: theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+    boxShadow:
+      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+    '& .MuiMenu-list': {
+      padding: '4px 0',
+    },
+    '& .MuiMenuItem-root': {
+      '& .MuiSvgIcon-root': {
+        fontSize: 16,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      '&:active': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity,
+        ),
+      },
+    },
+  },
+}));
+
+export default function DropDownMenu() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [selectedOption, setSelectedOption] = React.useState('Today'); 
+  const [options, setOptions] = React.useState(['Yesterday', 'Month', 'Year']);
+  
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  
+  const handleClose = (option?: string) => {
+    setAnchorEl(null);
+    if (option) {
+      setOptions(prevOptions => {
+        const updatedOptions = [...prevOptions, selectedOption];
+        return updatedOptions.filter(opt => opt !== option);
+      });
+      setSelectedOption(option);
+    }
+  };
+
+  return (
+    <div>
+      <Button
+        className={styles.buttoncolor}
+        id="demo-customized-button"
+        aria-controls={open ? 'demo-customized-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        variant="contained"
+        disableElevation
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon sx={{ color: '#C8161D' }} />}
+      >
+        {selectedOption}
+      </Button>
+      <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          'aria-labelledby': 'demo-customized-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={() => handleClose()}
+      >
+        {options.map(option => (
+          <MenuItem key={option} onClick={() => handleClose(option)} disableRipple>
+            {option}
+          </MenuItem>
+        ))}
+      </StyledMenu>
+    </div>
+  );
+}
--- src/app/(DataView)/DashBoard/page.tsx
+++ src/app/(DataView)/DashBoard/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import styles from './page.module.css'
+import DropDownMenu from '@/Components/Large/DropDownMenu/DropDownMenu'
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className={styles.dashboard}>
+      <div className={styles.dashboard_container}>
+        <div className={styles.dashboard_container_header}>
+          <span>Dashboard</span>
+          <DropDownMenu/>
+
+        </div>
+        <div className={styles.dashboard_container_analytics}>
+          
+        </div>
+        <div className={styles.dashboard_container_history}>
+          
+        </div>
+
+      </div>
+    </div>
   )
 }
 
